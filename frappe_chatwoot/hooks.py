38648ebf72d8ecc10507774d69da7eb285fee074
Aryan Singh <aquiveal@gmail.com>
2026-02-23 21:42:16 +0530

    feat: initialize frappe chatwoot app
    Initialize the project structure for the Frappe Chatwoot application.
    
    Add application configuration in hooks.py and define project metadata in pyproject.toml. Set up the development environment with configurations for pre-commit hooks, Ruff, ESLint, and EditorConfig. Include initial documentation, licensing, and standard Frappe directory organization.

---
+++ frappe_chatwoot/hooks.py
@@ -0,0 +1,255 @@
+app_name = "frappe_chatwoot"
+app_title = "Frappe Chatwoot"
+app_publisher = "Aryan Singh"
+app_description = "Frappe Chatwoot"
+app_email = "aquiveal@gmail.com"
+app_license = "mit"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "frappe_chatwoot",
+# 		"logo": "/assets/frappe_chatwoot/logo.png",
+# 		"title": "Frappe Chatwoot",
+# 		"route": "/frappe_chatwoot",
+# 		"has_permission": "frappe_chatwoot.api.permission.has_app_permission"
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/frappe_chatwoot/css/frappe_chatwoot.css"
+# app_include_js = "/assets/frappe_chatwoot/js/frappe_chatwoot.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/frappe_chatwoot/css/frappe_chatwoot.css"
+# web_include_js = "/assets/frappe_chatwoot/js/frappe_chatwoot.js"
+
+# include custom scss in every website theme (without file extension ".scss")
+# website_theme_scss = "frappe_chatwoot/public/scss/website"
+
+# include js, css files in header of web form
+# webform_include_js = {"doctype": "public/js/doctype.js"}
+# webform_include_css = {"doctype": "public/css/doctype.css"}
+
+# include js in page
+# page_js = {"page" : "public/js/file.js"}
+
+# include js in doctype views
+# doctype_js = {"doctype" : "public/js/doctype.js"}
+# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "frappe_chatwoot/public/icons.svg"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+# 	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# automatically load and sync documents of this doctype from downstream apps
+# importable_doctypes = [doctype_1]
+
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "frappe_chatwoot.utils.jinja_methods",
+# 	"filters": "frappe_chatwoot.utils.jinja_filters"
+# }
+
+# Installation
+# ------------
+
+# before_install = "frappe_chatwoot.install.before_install"
+# after_install = "frappe_chatwoot.install.after_install"
+
+# Uninstallation
+# ------------
+
+# before_uninstall = "frappe_chatwoot.uninstall.before_uninstall"
+# after_uninstall = "frappe_chatwoot.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "frappe_chatwoot.utils.before_app_install"
+# after_app_install = "frappe_chatwoot.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "frappe_chatwoot.utils.before_app_uninstall"
+# after_app_uninstall = "frappe_chatwoot.utils.after_app_uninstall"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "frappe_chatwoot.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"frappe_chatwoot.tasks.all"
+# 	],
+# 	"daily": [
+# 		"frappe_chatwoot.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"frappe_chatwoot.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"frappe_chatwoot.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"frappe_chatwoot.tasks.monthly"
+# 	],
+# }
+
+# Testing
+# -------
+
+# before_tests = "frappe_chatwoot.install.before_tests"
+
+# Extend DocType Class
+# ------------------------------
+#
+# Specify custom mixins to extend the standard doctype controller.
+# extend_doctype_class = {
+# 	"Task": "frappe_chatwoot.custom.task.CustomTaskMixin"
+# }
+
+# Overriding Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "frappe_chatwoot.event.get_events"
+# }
+#
+# each overriding function accepts a `data` argument;
+# generated from the base implementation of the doctype dashboard,
+# along with any modifications made in other Frappe apps
+# override_doctype_dashboards = {
+# 	"Task": "frappe_chatwoot.task.get_dashboard_data"
+# }
+
+# exempt linked doctypes from being automatically cancelled
+#
+# auto_cancel_exempted_doctypes = ["Auto Repeat"]
+
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["frappe_chatwoot.utils.before_request"]
+# after_request = ["frappe_chatwoot.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["frappe_chatwoot.utils.before_job"]
+# after_job = ["frappe_chatwoot.utils.after_job"]
+
+# User Data Protection
+# --------------------
+
+# user_data_fields = [
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
+# ]
+
+# Authentication and authorization
+# --------------------------------
+
+# auth_hooks = [
+# 	"frappe_chatwoot.auth.validate"
+# ]
+
+# Automatically update python controller files with type annotations for this app.
+export_python_type_annotations = True
+
+# Require all whitelisted methods to have type annotations
+require_type_annotated_api_methods = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
+
+# Translation
+# ------------
+# List of apps whose translatable strings should be excluded from this app's translations.
+# ignore_translatable_strings_from = []
+
